f72f207a07d6e4dd5d35b13cce0bbe01a340b29d
Marekby <marekby@gmail.com>
2023-03-21 21:27:17 +0100

    add all

---
+++ src/main/java/pl/sdacademy/converter/MorseCodeDecoder.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeDecoder {
+
+    private final MorseCodeEncoder dictionary;
+
+    public MorseCodeDecoder(MorseCodeEncoder dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public String decode(String morseCode) {
+        return "";
+    }
+}
